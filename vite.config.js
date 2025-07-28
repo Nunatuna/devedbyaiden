995bbe6034f689e71dfbbdbcde3fa274acf49615
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 17:22:00 +0200

    bug fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '.', // Copy index.html → dist/404.html
+          rename: '404.html'
+        }
+      ]
+    })
+  ],
     base: '/devedbyaiden/', // <-- replace with your repo name
 });
