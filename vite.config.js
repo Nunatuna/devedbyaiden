ec7d59c65d37d31f3b273078a08ba08d94ba4550
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 17:19:16 +0200

    setup for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite(), react()],
+    base: '/devedbyaiden/', // <-- replace with your repo name
 });
