97b78ceaa0edd401708053e947c31d4effdd45fe
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 18:13:54 +0200

    final try for today

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig(({ command }) => ({
   base: command === 'build' ? '/devedbyaiden/' : '/',
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'index.html',
-          dest: '.',
-          rename: '404.html',
-        },
-      ],
-    }),
   ],
 }));
