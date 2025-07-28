c8166663761d264f137ec0e8d7fe5eab581b00b2
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-29 01:54:29 +0200

    code reset for troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/devedbyaiden/' : '/',
-  plugins: [
-    react(),
-  ],
-}));
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [TanStackRouterVite(), react()],
+});
