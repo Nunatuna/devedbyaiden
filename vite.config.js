0388ca856d1dd1fa6a5c421ad8e0e097ebe0f2db
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 17:28:25 +0200

    troubleshooting

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/devedbyaiden/',
   plugins: [
     react(),
     viteStaticCopy({
       targets: [
         {
           src: 'index.html',
-          dest: '.', // Copy index.html → dist/404.html
-          rename: '404.html'
-        }
-      ]
-    })
+          dest: '.',
+          rename: '404.html',
+        },
+      ],
+    }),
   ],
-    base: '/devedbyaiden/', // <-- replace with your repo name
 });
