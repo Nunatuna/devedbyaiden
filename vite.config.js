5977ea248fdaa150da8c4542b4d000624c3857ac
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 17:23:21 +0200

    *sigh* one more bug fix

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 
 // https://vite.dev/config/
