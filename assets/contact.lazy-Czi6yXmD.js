aa34ea3e596e55eca977c7b5e675cab0671b41bd
Nunatuna <aiden.0407@hotmail.com.com>
2025-07-28 17:23:40 +0200

    Updates

---
+++ assets/contact.lazy-Czi6yXmD.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-Dz1PdMXf.js";const n=o("/contact")({component:t});function t(){return"Hello /contact!"}export{n as Route};
---
+++ assets/projects.lazy-BDQieJiD.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-Dz1PdMXf.js";const r=o("/projects")({component:t});function t(){return e.jsx("div",{children:e.jsx("h1",{children:"Projects"})})}export{r as Route};
