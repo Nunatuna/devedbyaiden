d10318407b7de7d28e6fbb0f1c125ec21f54bf23
Nunatuna <aiden.0407@hotmail.com.com>
2025-08-07 18:29:20 +0200

    Updates

---
+++ assets/contact.lazy-BDcBz6dW.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-BMK_2dBe.js";const n=o("/contact")({component:t});function t(){return"Hello /contact!"}export{n as Route};
---
+++ assets/projects.lazy-u6mtF1SB.js
@@ -0,0 +1,1 @@
+import{c as o,j as e}from"./index-BMK_2dBe.js";const r=o("/projects")({component:t});function t(){return e.jsx("div",{children:e.jsx("h1",{children:"Projects"})})}export{r as Route};
